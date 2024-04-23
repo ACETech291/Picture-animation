7cbf316e8fbbf0895f537f6f2baa3a3e41ee88e5
ACETech291 <hiepgacute1989@gmail.com>
2024-04-23 16:43:04 +0700

    first commit

--- index.js
+++ index.js
@@ -2,18 +2,23 @@ var video = document.getElementById('animatedVideo');
 var audio = document.getElementById('audio');
 var instructions = document.querySelector('.instructions');
 
-function startInteraction() {
+function startInteraction(event) {
+    event.preventDefault();
     video.style.filter = 'none';
+    video.style.pointerEvents = 'auto';
     video.play();
     audio.play();
     instructions.style.display = 'none';
 }
 
-function endInteraction() {
+function endInteraction(event) {
+    event.preventDefault();
     video.style.filter = 'grayscale(100%)';
+    video.style.pointerEvents = 'none';
     audio.pause();
     instructions.style.display = 'block';
 }
+
 
 window.addEventListener('mousedown', startInteraction);
 window.addEventListener('mouseup', endInteraction);
